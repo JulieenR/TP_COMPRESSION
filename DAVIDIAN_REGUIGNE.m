98ed5f0f978ad900a56e892c83f83403ed9a2d89
Your Name <you@example.com>
2020-03-20 15:16:49 +0100

    bck

--- DAVIDIAN_REGUIGNE.m
+++ DAVIDIAN_REGUIGNE.m
@@ -15,11 +15,33 @@ w = (sig(J)-sig(I))/2;
 %figure(3);
 subplot(1,2,2);plot(w);
 
-<<<<<<< HEAD
 %% Question 2.2
 
 load("PieceRegSig")
 load("Daub4")
-=======
-%% Question
->>>>>>> 90962174e7904c9f310cfac4f95a13c49ab08b33
+% Pour e
+
+e_conv=conv(sig, rh);
+e1=e_conv(1:2:length(e_conv));
+
+%Pour w
+
+w_conv=conv(sig, rg);
+w1=w_conv(1:2:length(w_conv));
+
+%Affichage 
+figure(3)
+subplot(1,2,1)
+plot(e1);
+title("e1 - Résultat du sous échantillonage de la convolution du signal avec rh");
+xlabel("Echantillons");
+ylabel("Amplitude");
+subplot(1,2,2);
+plot(w1);
+title("w1 - Résultat du sous échantillonage de la convolution du signal avec rg");
+xlabel("Echantillons");
+ylabel("Amplitude");
+
+
+
+%% Question 2.3
